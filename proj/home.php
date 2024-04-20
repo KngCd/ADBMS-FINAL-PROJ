03bef6afae8758c289ead910e11b5788912c0052
kil_zldyck <148508745+KngCd@users.noreply.github.com>
2024-04-20 09:26:07 +0800

    Update home.php

--- proj/home.php
+++ proj/home.php
@@ -6,33 +6,81 @@
     <title>Pansol Integrated National High School</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/freeps2/a7rarpress@main/swiper-bundle.min.css">
+    <script src="//cdn.jsdelivr.net/gh/freeps2/a7rarpress@main/swiper-bundle.min.js"></script>
+    <script src="//cdn.jsdelivr.net/gh/freeps2/a7rarpress@main/script.js"></script>
 </head>
 <body>
     <header id="myHeader" class>
     <div class="head" id="header">
     <div class="h2"><img class="logo" src="images/schooal.png" alt=""><p class="sc">Pansol Integrated <br>National High School<p/></div>
-            <div class="h3">
-            <a class="nav-link" href="#content-container">HOME</a>
-            <a class="nav-link" href="#about">ABOUT</a>
-            <a class="nav-link" href="#contact">CONTACT US</a>
+    
+            <div class="topnav" id="myTopnav">
+                <a href="#content-container">HOME</a>
+                <a href="#about">ABOUT</a>
+                <a href="#contact">CONTACT US</a>
+                <a onclick="location.href='LoginSignup.php'"><button class="button">LOGIN</button></a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars" style="font-size: 2em;"></i>
+                </a>
             </div>
-            <button onclick="location.href='LoginSignup.php'" class="bn54">
-                <span class="bn54span">Login</span>
-            </button>
+            <script>
+                function myFunction() {
+                var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } else {
+                    x.className = "topnav";
+                }
+                }
+                </script>
+
     </div>
     </header>
-        <script>
-            window.addEventListener('scroll', function() {
-            const header = document.getElementById('myHeader');
+    <script>const contentContainer = document.querySelector('.content-container');
+
+        const handleScroll = () => {
+        const scrollPosition = window.pageYOffset;
+        const containerHeight = contentContainer.offsetHeight;
+        const imageHeight = contentContainer.offsetHeight;
+
+        const scrollPercentage = scrollPosition / (containerHeight - window.innerHeight);
+        const imageOffset = Math.round(imageHeight * scrollPercentage);
+
+        contentContainer.style.backgroundPositionY = `${imageOffset}px`;
+        };
+        </script>
+       <script>
+        window.addEventListener('scroll', function() {
+            const headerLinks = document.querySelectorAll('#myHeader .topnav a'); // Select all the a elements inside the .topnav div
             const scrollPosition = window.pageYOffset;
 
             if (scrollPosition > 50) {
-                header.style.backgroundColor = 'rgba(30, 97, 243, 0.836)';
+                headerLinks.forEach(link => {
+                    link.style.color = 'black'; // Change the font color to black
+                });
             } else {
-                header.style.backgroundColor = 'transparent';
+                headerLinks.forEach(link => {
+                    link.style.color = ''; // Reset the font color to its default value
+                });
             }
-            });
+            const headerLinks1 = document.querySelectorAll('#myHeader .h2 p'); // Select all the a elements inside the .topnav div
+            const scrollPosition1 = window.pageYOffset;
+
+            if (scrollPosition1 > 50) {
+                headerLinks1.forEach(link => {
+                    link.style.color = 'black'; // Change the font color to black
+                });
+            } else {
+                headerLinks1.forEach(link => {
+                    link.style.color = ''; // Reset the font color to its default value
+                });
+            }
+        });
         </script>
+
 <div class="content-container" id="content-container">
             <div class="h-container">
                 <p class="paragraph">
@@ -119,6 +167,191 @@
             </div>
         </div>
     </div>
+
+                <div class="responsive-container-block outer-container">
+            <div class="responsive-container-block inner-container">
+                <p class="text-blk section-head-text">
+                Our Team
+                </p>
+                <p class="text-blk section-subhead-text">
+                Meet the brilliant minds behind our project - a team of talented students dedicated to creating innovative systems and solutions.
+                </p>
+            <div class="responsive-container-block">
+                <div class="swiper team-swiper">
+                    <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="cards">
+                        <div class="img-wrapper">
+                            <img src="images/barbs.jpg">
+                        </div>
+                        <div class="card-content">
+                            <p class="text-blk name">
+                            John Wilfred P. Abratique
+                            </p>
+                            <p class="text-blk position">
+                            Student
+                            </p>
+                            <p class="text-blk testimonial">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="cards">
+                        <div class="img-wrapper">
+                            <img src="images/mark.png">
+                        </div>
+                        <div class="card-content">
+                            <p class="text-blk name">
+                            Mark Laurence L. Caringal
+                            </p>
+                            <p class="text-blk position">
+                            Student
+                            </p>
+                            <p class="text-blk testimonial">
+                            "When in doubt, just go with the flow; answers reveal themselves along the journey."
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="cards">
+                        <div class="img-wrapper">
+                            <img src="images/kc.jpg">
+                        </div>
+                        <div class="card-content">
+                            <p class="text-blk name">
+                            Keith Andrei A. Ciruelas
+                            </p>
+                            <p class="text-blk position">
+                            Student
+                            </p>
+                            <p class="text-blk testimonial">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="cards">
+                        <div class="img-wrapper">
+                            <img src="images/lourenz.jpg">
+                        </div>
+                        <div class="card-content">
+                            <p class="text-blk name">
+                            Lourenz Angel A. Francisco
+                            </p>
+                            <p class="text-blk position">
+                            Student
+                            </p>
+                            <p class="text-blk testimonial">
+                            "God knew I'd be too powerful if I wasn't both procastinator and perfectionist
+                            at the same time."
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="cards">
+                        <div class="img-wrapper">
+                            <img src="images/jerome.jpg">
+                        </div>
+                        <div class="card-content">
+                            <p class="text-blk name">
+                            King Jerome P. Lacerna
+                            </p>
+                            <p class="text-blk position">
+                             Student
+                            </p>
+                            <p class="text-blk testimonial">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="cards">
+                        <div class="img-wrapper">
+                            <img src="images/von.jpg">
+                        </div>
+                        <div class="card-content">
+                            <p class="text-blk name">
+                            Von Cedric R. Latag
+                            </p>
+                            <p class="text-blk position">
+                            Student
+                            </p>
+                            <p class="text-blk testimonial">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="cards">
+                        <div class="img-wrapper">
+                            <img src="images/ced.jpg">
+                        </div>
+                        <div class="card-content">
+                            <p class="text-blk name">
+                            King Cedrick C. Panaligan
+                            </p>
+                            <p class="text-blk position">
+                            Student
+                            </p>
+                            <p class="text-blk testimonial">
+                            "Like butterflies unaware of their own beauty, you might not see your brilliance, but others do."
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="swiper-pagination container-block">
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+
+        </div>
+                <script>
+                    var swiper = new Swiper(".team-swiper", {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                    loop: true,
+                    pagination: {
+                        el: ".swiper-pagination",
+                        clickable: true,
+                    },
+                    autoplay: {
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    },
+                    breakpoints: {
+                        200: {
+                        slidesPerView: 1,
+                        spaceBetween: 20
+                        },
+                        750: {
+                        slidesPerView: 2,
+                        spaceBetween: 30
+                        },
+                        1024: {
+                        slidesPerView: 2.5,
+                        spaceBetween: 30
+                        },
+                        1100: {
+                        slidesPerView: 3,
+                        spaceBetween: 30
+                        },
+                    }
+                    });
+                </script>
         <script>
             document.querySelectorAll('.content-mission').forEach(card => {
                 card.addEventListener('mouseover', () => {
